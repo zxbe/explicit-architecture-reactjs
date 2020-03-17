33432daec0b5ad8025c5765d089748d0bf5d19bc
Herberto Graca <herberto.graca@gmail.com>
2020-03-17 21:17:19 +0100

    Style the content section

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import { Route, Switch } from 'react-router-dom'
 import GistListPage from "./Core/Actor/Visitor/GistListPage";
 import GistPage from "./Core/Actor/Visitor/GistPage";
 import Header from "./Core/SharedKernel/Organism/Header";
-import { Content, ContentRow, ContentRowMain, Header, Sidebar } from "./App.styled";
+import { Content, ContentRow, ContentRowMain, Sidebar } from "./App.styled";
 
 const App = () => (
   <ApolloProvider client={ GitHubGqlClient }>
     <Header />
     <Content>
-      <Switch>
-        <Route exact path="/" component={ GistListPage }/>
-        <Route path="/gist/:name" component={ GistPage }/>
-      </Switch>
+      <ContentRow>
+        <ContentRowMain>
+          <Switch>
+            <Route exact path="/" component={ GistListPage }/>
+            <Route path="/gist/:name" component={ GistPage }/>
+          </Switch>
+        </ContentRowMain>
+        <Sidebar></Sidebar>
+      </ContentRow>
     </Content>
   </ApolloProvider>
 );
